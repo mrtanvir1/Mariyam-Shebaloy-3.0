74a674b61322456384447b98408b046f5f651300
mrtanvir1 <138609757+mrtanvir1@users.noreply.github.com>
2026-09-20 14:53:42 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mariyam-shebaloy-v9-prescription";
+const CACHE = "mariyam-shebaloy-v9";
 const SHELL = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./config.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
